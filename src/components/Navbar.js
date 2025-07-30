12c9d74152da5ebbc5257f23707132542bb117a5
mohamed medhat <73649080+CoDeBrEaKe@users.noreply.github.com>
2025-07-31 00:07:18 +0300

    Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import data from "./../data/data.json";
-
+import { useEffect } from "react";
 import icon from "./../assets/shared/icon-hamburger.svg";
 import close from "./../assets/shared/icon-close.svg";
 import Logo from "./../assets/shared/logo.svg";
@@ -38,6 +38,39 @@ const Navbar = ({ page, setPage }) => {
     }
     x++;
   }
+  useEffect(() => {
+    // Your burger icon handling code here
+    let list = document.querySelector("header ul");
+    let burgerMenu = document.querySelectorAll("header .small")[0];
+    let closeMenu = document.querySelectorAll("header .small")[1];
+    // let content = document.querySelector('header + div');
+
+    burgerMenu.addEventListener("click", () => {
+      list.style.display = "flex";
+
+      burgerMenu.classList.remove("active");
+      setTimeout(() => {
+        burgerMenu.style.display = "none";
+        closeMenu.classList.add("active");
+        list.style.opacity = 1;
+        closeMenu.style.opacity = 1;
+      }, 100);
+    });
+    closeMenu.addEventListener("click", () => {
+      closeMenu.style.opacity = 0;
+      list.style.opacity = 0;
+      setTimeout(() => {
+        burgerMenu.classList.add("active");
+        closeMenu.classList.remove("active");
+
+        list.style.display = "none";
+        burgerMenu.style.display = "block";
+      }, 400);
+    });
+
+    // Cleanup function to remove event listeners
+  }, []); // Empty dependency array means this runs once after initial render
+
   return (
     <header>
       <img src={Logo} />
