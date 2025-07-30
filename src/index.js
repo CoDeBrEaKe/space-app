e93f3617474cad7773a81557420f729223bdda7e
mohamed medhat <73649080+CoDeBrEaKe@users.noreply.github.com>
2025-07-30 23:47:08 +0300

    Fixed some layout issues

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const script = document.createElement("script");
-    script.src = "style.js";
-    script.async = true;
-    document.body.appendChild(script);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
